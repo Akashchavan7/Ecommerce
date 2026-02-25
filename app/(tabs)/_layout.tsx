6eaf81eae5ba3eb4e73328e7cf89ab5450915341
Aakash Chavan <akash73.chavan@gmail.com>
2026-02-25 14:36:58 +0530

    feat: floating bottom navigation bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,29 +1,76 @@
 import { Tabs } from "expo-router";
+import { View,StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout(){
 
 return(
 
-<Tabs screenOptions={{headerShown:false}}>
+<Tabs
+
+screenOptions={{
+
+headerShown:false,
+
+tabBarShowLabel:false,
+
+tabBarStyle:styles.tabBar
+
+}}
+
+>
 
 <Tabs.Screen
 name="index"
 options={{
-title:"Home"
+tabBarIcon:({focused})=>(
+<View style={[
+styles.iconBox,
+focused && styles.active
+]}>
+<Ionicons
+name="home"
+size={22}
+color={focused?"#fff":"#555"}
+/>
+</View>
+)
 }}
 />
 
 <Tabs.Screen
 name="categories"
 options={{
-title:"Categories"
+tabBarIcon:({focused})=>(
+<View style={[
+styles.iconBox,
+focused && styles.active
+]}>
+<Ionicons
+name="grid"
+size={22}
+color={focused?"#fff":"#555"}
+/>
+</View>
+)
 }}
 />
 
 <Tabs.Screen
 name="profile"
 options={{
-title:"Profile"
+tabBarIcon:({focused})=>(
+<View style={[
+styles.iconBox,
+focused && styles.active
+]}>
+<Ionicons
+name="person"
+size={22}
+color={focused?"#fff":"#555"}
+/>
+</View>
+)
 }}
 />
 
@@ -32,3 +79,39 @@ title:"Profile"
 );
 
 }
+
+const styles=StyleSheet.create({
+
+tabBar:{
+position:"absolute",
+bottom:20,
+left:20,
+right:20,
+
+height:65,
+
+borderRadius:35,
+
+backgroundColor:"#ffffff",
+
+shadowColor:"#000",
+shadowOpacity:0.15,
+shadowRadius:15,
+shadowOffset:{width:0,height:10},
+
+elevation:10
+},
+
+iconBox:{
+width:45,
+height:45,
+borderRadius:22,
+justifyContent:"center",
+alignItems:"center"
+},
+
+active:{
+backgroundColor:"#4facfe"
+}
+
+});
