5a11bf7bb5025f25b895406df7530059465bf16f
Aakash Chavan <akash73.chavan@gmail.com>
2026-02-25 14:22:04 +0530

    feat: add profile screen

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,20 @@
+import { View,Text,StyleSheet } from "react-native";
+
+export default function Profile(){
+
+return(
+
+<View style={styles.container}>
+<Text>User Profile</Text>
+</View>
+
+);
+}
+
+const styles=StyleSheet.create({
+container:{
+flex:1,
+justifyContent:"center",
+alignItems:"center"
+}
+});
